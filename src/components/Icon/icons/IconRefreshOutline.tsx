e83764d537e762ee525590ac85a8730be15c6a61
bhenique <faire.des.economies.energie@gmail.com>
2024-11-23 18:21:03 +0100

    feat: add refresh outline icon

---
+++ src/components/Icon/icons/IconRefreshOutline.tsx
@@ -0,0 +1,39 @@
+import { IconProps } from '@qwikest/icons'
+import { updateFillOfChildren } from '~/utils/update-icon-fill'
+import { component$, _jsxQ, _jsxS, type JSXNode } from '@builder.io/qwik'
+
+export const IconRefreshOutline = component$<IconProps>((props) => {
+  const ic = _jsxS(
+    'svg',
+    {
+      ...props,
+      children: _jsxQ(
+        'path',
+        null,
+        {
+          d: 'M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4',
+          stroke: 'currentColor',
+          'stroke-linecap': 'round',
+          'stroke-linejoin': 'round',
+          'stroke-width': '2',
+        },
+        null,
+        3,
+        null,
+      ),
+    },
+    {
+      'data-qwikest-icon': true,
+      fill: 'none',
+      height: '1em',
+      viewBox: '0 0 24 24',
+      width: '1em',
+      xmlns: 'http://www.w3.org/2000/svg',
+    },
+    0,
+    '1u_09',
+  )
+
+  ic.children = updateFillOfChildren(ic.children as JSXNode, 'currentColor')
+  return <>{ic}</>
+})
